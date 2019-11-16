68a713340e59390744ef97904b03684ad59f1d10
Jason Kennemer <jason@kennemers.com>
2019-11-15 18:06:05 -0600

    updates.sh add

---
+++ updates.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+function up
+    sudo softwareupdate --install --all
+    brew update; brew upgrade; brew prune; brew cleanup; brew doctor;
+    pip3 install --upgrade pip setuptools wheel
+    omf install; omf update; omf reload; omf doctor;
+end
