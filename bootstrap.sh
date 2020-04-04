6a20f8e0c05bf3c910a41058869f8bc9ed112290
Jason Kennemer <jason@kennemers.com>
2020-04-03 19:18:47 -0500

    color customization and more powerline cleanout

--- bootstrap.sh
+++ bootstrap.sh
@@ -249,17 +249,15 @@ function setup_tmux() {
     fi
     success "tmux successfully setup"
 }
-
+1.0825
 function setup_symlinks() {
     APPLICATION_SUPPORT=~/Library/Application\ Support
-    POWERLINE_ROOT_REPO=/usr/local/lib/python3.7/site-packages
 
     info "Setting up symlinks"
     symlink "git" ${DOTFILES_REPO}/git/gitconfig ~/.gitconfig
     symlink "gitignore" ${DOTFILES_REPO}/git/gitignore ~/.gitignore
     symlink "hammerspoon" ${DOTFILES_REPO}/hammerspoon ~/.hammerspoon
     symlink "karabiner" ${DOTFILES_REPO}/karabiner ~/.config/karabiner
-    symlink "powerline" ${DOTFILES_REPO}/powerline ${POWERLINE_ROOT_REPO}/powerline/config_files
     symlink "tmux" ${DOTFILES_REPO}/tmux/tmux.conf ~/.tmux.conf
     symlink "vim" ${DOTFILES_REPO}/vim/vimrc ~/.vimrc
 
